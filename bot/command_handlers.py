8c5127ef61ddbf259c4a2bcf7a1add219f267fd1
shichchirtch <ivanvol9l@gmail.com>
2024-09-10 21:24:43 +0200

    6 Commit
    двухсложные слова в английском 2

--- bot/command_handlers.py
+++ bot/command_handlers.py
@@ -91,6 +91,7 @@ async def artikle_geber(message: Message):
                 eng_analog = english_gleiche[1].text
                 print('english_gleiche = ', english_gleiche)
                 gleiche = eng_analog.split()[-1].capitalize()
+                print('eng_analog.split()[-1] = ', eng_analog.split()[-1])
 
                 if plural_data:
                     atw_satz = f'<b>{neue_wort};</b>  Plural Form  <b>{plural_data}</b>\n<b><i>English = {gleiche}</i></b>'
